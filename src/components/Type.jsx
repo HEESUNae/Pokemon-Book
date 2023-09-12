c8139c7b72c50d0b4b43a1f913f419ee655ad7a9
HEESUNae <heesun9456@gmail.com>
2023-09-12 22:31:20 +0900

    feat: 포켓몬 상세페이지 완료

---
+++ src/components/Type.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Type = ({ type, demageValue }) => {
+  const bg = `bg-${type}`;
+  return (
+    <div>
+      <span
+        className={`h-[1.5rem] py-1 px-3 rounded-2xl ${bg} font-bold text-zinc-800 text-[0.6rem] leading=[0.8rem] capitalize flex gap-1 justify-center items-center`}
+      >
+        {type}
+      </span>
+      {demageValue && <span className="bg-zinc-200/40 p-[.125rem] rounded">{demageValue}</span>}
+    </div>
+  );
+};
+
+export default Type;
